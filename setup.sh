74b15702004ab2bcb22608afae52e60eee20cb21
Louis BENOIST--FOUCHER <louis.benoist-foucher@estiam.com>
2018-10-09 15:49:09 +0200

    no sudo no global

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ git clone --depth 1  https://github.com/batteurMDR/shm-server-front.git -b dev s
 cd ./shm-server-back
 npm install
 cd ../shm-server-front
-sudo npm install pngquant-bin
+npm install pngquant-bin
 npm install pngquant
 npm install
 
